fc01f3fc94909a43a46747d616fe60b287266fc9
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-01-09 16:08:49 -0300

    Projeto criado

---
+++ src/main/java/com/antunes/picpayfinanceservice/PicpayFinanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.antunes.picpayfinanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayFinanceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicpayFinanceServiceApplication.class, args);
+    }
+
+}
