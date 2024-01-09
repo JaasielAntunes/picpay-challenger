3576b2bab8164d08f28ddd35fc7986eb8a50ed38
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-01-09 16:30:48 -0300

    Concluindo a classe User inserindo um UserType

--- src/main/java/com/antunes/picpayfinanceservice/domain/user/User.java
+++ src/main/java/com/antunes/picpayfinanceservice/domain/user/User.java
@@ -10,6 +10,8 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Entity
 @Table(name = "users")
 @Getter
@@ -39,4 +41,17 @@ public class User {
     @Email(message = "Email inválido!")
     @NotEmpty(message = "O campo não pode ser vazio!")
     private String email;
+
+    @NotEmpty(message = "O campo não pode ser vazio!")
+    @Min(value = 7, message = "O campo deve conter entre 7 e 20 caracteres!")
+    @Max(value = 20, message = "O campo deve conter entre 7 e 20 caracteres!")
+    private String password;
+
+    @NotEmpty(message = "O campo não pode ser vazio!")
+    @Min(value = 4, message = "O campo deve conter entre 4 e 20 caracteres!")
+    @Max(value = 10, message = "O campo deve conter entre 4 e 20 caracteres!")
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
 }
