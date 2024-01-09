138fbe9be73b09d195c6737273db8e08fe4fa4f5
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-01-09 16:12:32 -0300

    Criação da classe User utilizando Validation

---
+++ src/main/java/com/antunes/picpayfinanceservice/domain/user/User.java
@@ -0,0 +1,42 @@
+package com.antunes.picpayfinanceservice.domain.user;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "O campo não pode ser vazio!")
+    @Min(value = 4, message = "O campo deve conter entre 4 e 10 caracteres!")
+    @Max(value = 10, message = "O campo deve conter entre 4 e 10 caracteres!")
+    private String firstName;
+
+    @NotEmpty(message = "O campo não pode ser vazio!")
+    @Min(value = 4, message = "O campo deve conter entre 4 e 20 caracteres!")
+    @Max(value = 10, message = "O campo deve conter entre 4 e 20 caracteres!")
+    private String lastName;
+
+    @Column(unique = true)
+    @NotEmpty(message = "O campo não pode ser vazio!")
+    private String document;
+    @Column(unique = true)
+    @Email(message = "Email inválido!")
+    @NotEmpty(message = "O campo não pode ser vazio!")
+    private String email;
+}
